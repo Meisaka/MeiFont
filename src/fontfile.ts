c0970e1451f140d36940782876a7547f73ca5f3f
Meisaka Yukara <Meisaka.Yukara@gmail.com>
2025-10-02 21:55:03 -0500

    add standard sids; cmap and head tables
    handles unicode encodings in the cmap

--- src/fontfile.ts
+++ src/fontfile.ts
@@ -28,6 +28,82 @@ const font_name_ids = [
 	/* 25 */'postscript_prefix',
 ]
 
+const font_std_sids = [
+'.notdef',
+'space', 'exclam', 'quotedbl', 'numbersign', 'dollar',
+'percent', 'ampersand', 'quoteright', 'parenleft', 'parenright',
+'asterisk', 'plus', 'comma', 'hyphen', 'period',
+'slash', 'zero', 'one', 'two', 'three',
+'four', 'five', 'six', 'seven', 'eight',
+'nine', 'colon', 'semicolon', 'less', 'equal',
+'greater', 'question', 'at', 'A', 'B', 'C', 'D', 'E', 'F', 'G',
+'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V',
+'W', 'X', 'Y', 'Z', 'bracketleft',
+'backslash', 'bracketright', 'asciicircum', 'underscore', 'quoteleft',
+'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o',
+'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y',
+'z', 'braceleft', 'bar', 'braceright', 'asciitilde',
+'exclamdown', 'cent', 'sterling', 'fraction', 'yen',
+'florin', 'section', 'currency', 'quotesingle', 'quotedblleft',
+'guillemotleft', 'guilsinglleft', 'guilsinglright', 'fi', 'fl',
+'endash', 'dagger', 'daggerdbl', 'periodcentered', 'paragraph',
+'bullet', 'quotesinglbase', 'quotedblbase', 'quotedblright', 'guillemotright',
+'ellipsis', 'perthousand', 'questiondown', 'grave', 'acute',
+'circumflex', 'tilde', 'macron', 'breve', 'dotaccent',
+'dieresis', 'ring', 'cedilla', 'hungarumlaut', 'ogonek',
+'caron', 'emdash', 'AE', 'ordfeminine', 'Lslash',
+'Oslash', 'OE', 'ordmasculine', 'ae', 'dotlessi',
+'lslash', 'oslash', 'oe', 'germandbls', 'onesuperior',
+'logicalnot', 'mu', 'trademark', 'Eth', 'onehalf',
+'plusminus', 'Thorn', 'onequarter', 'divide', 'brokenbar',
+'degree', 'thorn', 'threequarters', 'twosuperior', 'registered',
+'minus', 'eth', 'multiply', 'threesuperior', 'copyright',
+'Aacute', 'Acircumflex', 'Adieresis', 'Agrave', 'Aring',
+'Atilde', 'Ccedilla', 'Eacute', 'Ecircumflex', 'Edieresis',
+'Egrave', 'Iacute', 'Icircumflex', 'Idieresis', 'Igrave',
+'Ntilde', 'Oacute', 'Ocircumflex', 'Odieresis', 'Ograve',
+'Otilde', 'Scaron', 'Uacute', 'Ucircumflex', 'Udieresis',
+'Ugrave', 'Yacute', 'Ydieresis', 'Zcaron', 'aacute',
+'acircumflex', 'adieresis', 'agrave', 'aring', 'atilde',
+'ccedilla', 'eacute', 'ecircumflex', 'edieresis', 'egrave',
+'iacute', 'icircumflex', 'idieresis', 'igrave', 'ntilde',
+'oacute', 'ocircumflex', 'odieresis', 'ograve', 'otilde',
+'scaron', 'uacute', 'ucircumflex', 'udieresis', 'ugrave',
+'yacute', 'ydieresis', 'zcaron', 'exclamsmall', 'Hungarumlautsmall',
+'dollaroldstyle', 'dollarsuperior', 'ampersandsmall', 'Acutesmall', 'parenleftsuperior',
+'parenrightsuperior', 'twodotenleader', 'onedotenleader', 'zerooldstyle', 'oneoldstyle',
+'twooldstyle', 'threeoldstyle', 'fouroldstyle', 'fiveoldstyle', 'sixoldstyle',
+'sevenoldstyle', 'eightoldstyle', 'nineoldstyle', 'commasuperior', 'threequartersemdash',
+'periodsuperior', 'questionsmall', 'asuperior', 'bsuperior', 'centsuperior',
+'dsuperior', 'esuperior', 'isuperior', 'lsuperior', 'msuperior',
+'nsuperior', 'osuperior', 'rsuperior', 'ssuperior', 'tsuperior',
+'ff', 'ffi', 'ffl', 'parenleftinferior', 'parenrightinferior',
+'Circumflexsmall', 'hyphensuperior', 'Gravesmall', 'Asmall', 'Bsmall',
+'Csmall', 'Dsmall', 'Esmall', 'Fsmall', 'Gsmall',
+'Hsmall', 'Ismall', 'Jsmall', 'Ksmall', 'Lsmall',
+'Msmall', 'Nsmall', 'Osmall', 'Psmall', 'Qsmall',
+'Rsmall', 'Ssmall', 'Tsmall', 'Usmall', 'Vsmall',
+'Wsmall', 'Xsmall', 'Ysmall', 'Zsmall', 'colonmonetary',
+'onefitted', 'rupiah', 'Tildesmall', 'exclamdownsmall', 'centoldstyle',
+'Lslashsmall', 'Scaronsmall', 'Zcaronsmall', 'Dieresissmall', 'Brevesmall',
+'Caronsmall', 'Dotaccentsmall', 'Macronsmall', 'figuredash', 'hypheninferior',
+'Ogoneksmall', 'Ringsmall', 'Cedillasmall', 'questiondownsmall', 'oneeighth',
+'threeeighths', 'fiveeighths', 'seveneighths', 'onethird', 'twothirds',
+'zerosuperior', 'foursuperior', 'fivesuperior', 'sixsuperior', 'sevensuperior',
+'eightsuperior', 'ninesuperior', 'zeroinferior', 'oneinferior', 'twoinferior',
+'threeinferior', 'fourinferior', 'fiveinferior', 'sixinferior', 'seveninferior',
+'eightinferior', 'nineinferior', 'centinferior', 'dollarinferior', 'periodinferior',
+'commainferior', 'Agravesmall', 'Aacutesmall', 'Acircumflexsmall', 'Atildesmall',
+'Adieresissmall', 'Aringsmall', 'AEsmall', 'Ccedillasmall', 'Egravesmall',
+'Eacutesmall', 'Ecircumflexsmall', 'Edieresissmall', 'Igravesmall', 'Iacutesmall',
+'Icircumflexsmall', 'Idieresissmall', 'Ethsmall', 'Ntildesmall', 'Ogravesmall',
+'Oacutesmall', 'Ocircumflexsmall', 'Otildesmall', 'Odieresissmall', 'OEsmall',
+'Oslashsmall', 'Ugravesmall', 'Uacutesmall', 'Ucircumflexsmall', 'Udieresissmall',
+'Yacutesmall', 'Thornsmall', 'Ydieresissmall', '001.000', '001.001',
+'001.002', '001.003', 'Black', 'Bold', 'Book',
+'Light', 'Medium', 'Regular', 'Roman', 'Semibold',
+]
+
 class CFFIndex {
 	count: number
 	buf: Uint8Array
@@ -90,6 +166,91 @@ class EEKError extends Error {
 		super(ohnoe);
 	}
 }
+const tt_platforms: (keyof typeof tt_platform_encodings)[] = [ 'unicode', 'macintosh', 'iso', 'windows', 'custom' ]
+const tt_platform_encodings = {
+	unknown: [],
+	unicode: [
+		'unicode1_0', 'unicode1_1', 'iso10646',
+		'unicode_bmp', 'unicode_full', 'unicode_var', 'unicode_full_13'
+	],
+	macintosh: [
+		/* 0 */ 'Roman',  /* 1 */ 'Japanese', /* 2 */ 'Chinese (Traditional)',
+		/* 3 */ 'Korean', /* 4 */ 'Arabic',   /* 5 */ 'Hebrew',
+		/* 6 */ 'Greek',  /* 7 */ 'Russian',  /* 8 */ 'RSymbol',
+		/* 9 */ 'Devanagari', /* 10 */ 'Gurmukhi', /* 11 */ 'Gujarati',
+		/* 12 */ 'Odia',      /* 13 */ 'Bangla',   /* 14 */ 'Tamil',
+		/* 15 */ 'Telugu',    /* 16 */ 'Kannada',  /* 17 */ 'Malayalam',
+		/* 18 */ 'Sinhalese', /* 19 */ 'Burmese',  /* 20 */ 'Khmer',
+		/* 21 */ 'Thai',      /* 22 */ 'Laotian',  /* 23 */ 'Georgian',
+		/* 24 */ 'Armenian', /* 25 */ 'Chinese (Simplified)',
+		/* 26 */ 'Tibetan', /* 27 */ 'Mongolian', /* 28 */ 'Geez',
+		/* 29 */ 'Slavic', /* 30 */ 'Vietnamese', /* 31 */ 'Sindhi',
+		/* 32 */ 'Uninterpreted',
+	],
+	iso: ['ascii7', 'iso10646', 'iso8859-1'],
+	windows: [
+		'Symbol', 'unicode_bmp', 'ShiftJIS', 'PRC', 'Big5', 'Wansung',
+		'Johab',
+		'!r7', '!r8', '!r9',
+		'unicode_full',
+	],
+	custom: [],
+}
+const TT_EPOCH = -3600000*24*(365*66+17)
+
+function parse_tt_head(table: FontTable, buf: Uint8Array) {
+	let view = new DataView(buf.buffer, buf.byteOffset, buf.byteLength)
+	let u16 = (offset: number) => view.getUint16(offset)
+	let i16 = (offset: number) => view.getInt16(offset)
+	let u32 = (offset: number) => view.getUint32(offset)
+	let version_maj = u16(0), version_min = u16(2)
+	let font_rev = u32(4), checksum_adj = u32(8)
+	let magic = u32(12)
+	let flags = u16(16), per_em = u16(18)
+	let create_hi = u32(20), create_lo = u32(24)
+	let modif_hi = u32(28), modif_lo = u32(32)
+	let created = new Date(TT_EPOCH + (create_lo * 1000) + (create_hi * 0x3e800000000))
+	let modifed = new Date(TT_EPOCH + (modif_lo * 1000) + (modif_hi * 0x3e800000000))
+	let min_x = i16(36), min_y = i16(38), max_x = i16(40), max_y = i16(42)
+	let mac_style = u16(44)
+	let lowest_ppem = u16(46)
+	let fdir_hint = i16(48)
+	let index_to_loc = u16(50)
+	let glyph_format = u16(52)
+	if(version_maj != 1 || version_min != 0 || magic != 0x5f0f3cf5 || fdir_hint != 2 || glyph_format != 0) {
+		console.error(EEK, `font 'head' table fields`)
+	}
+	console.log(`revision ${(font_rev/ 65536).toFixed(4)}`,
+		'min Pix/Em:', lowest_ppem, 'units/Em:', per_em
+	)
+	let font_flags:any = {}
+	if(mac_style & 1) font_flags.bold = true
+	if(mac_style & 2) font_flags.italic = true
+	if(mac_style & 4) font_flags.underline = true
+	if(mac_style & 8) font_flags.outline = true
+	if(mac_style & 16) font_flags.shadow = true
+	if(mac_style & 32) font_flags.condensed = true
+	if(mac_style & 64) font_flags.extended = true
+	if(flags & 1) font_flags.baseline_y_zero = true
+	if(flags & 2) font_flags.left_sidebearing_x_zero = true
+	if(flags & 4) font_flags.point_size = true
+	if(flags & 8) font_flags.int_scale = true
+	if(flags & 16) font_flags.advance_width = true
+	if(flags & 32) font_flags.vlayout = true
+	if(flags & 64) font_flags.do_not_set_this_bit_or_else = true
+	if(flags & 128) font_flags.layout_shenanigans = true
+	if(flags & 256) font_flags.font_is_a_butterfly = true
+	if(flags & 512) font_flags.rtl_headache_mode = true
+	if(flags & 1024) font_flags.indic_blender = true
+	if(flags & 2048) font_flags.lossless_oof = true
+	if(flags & 4096) font_flags.font_metrics_look_too_familiar = true
+	if(flags & 8192) font_flags.font_might_look_okay_if_blury = true
+	if(flags & 16384) font_flags.this_is_not_the_font_you_are_looking_for = true
+	if(flags & 32768) font_flags.font_is_made_of_pure_evil_and_should_be_excised = true
+	console.log('flags', font_flags)
+	console.log('c', created.toUTCString(), 'm', modifed.toUTCString())
+	console.log('bounds', min_x, min_y, '-', max_x, max_y)
+}
 
 function parse_tt_name(table: FontTable, buf: Uint8Array) {
 	function u16(offset: number) { return buf[offset] * 0x100 + buf[offset+1] }
@@ -111,8 +272,8 @@ function parse_tt_name(table: FontTable, buf: Uint8Array) {
 	// array of name records
 	for(let rec_index = 0; rec_index < count; rec_index++) {
 		let rec_offset = 6 + (rec_index * 12)
-		let platform = u16(rec_offset),
-			encoding = u16(rec_offset+2),
+		let platform = tt_platforms[u16(rec_offset)] ?? 'unknown',
+			encoding = tt_platform_encodings[platform][u16(rec_offset+2)] ?? `Enc#${u16(rec_offset+2)}`,
 			lang = u16(rec_offset+4),
 			id = u16(rec_offset+6),
 			length = u16(rec_offset+8),
@@ -120,7 +281,7 @@ function parse_tt_name(table: FontTable, buf: Uint8Array) {
 		let s = ''
 		//let meh: number[] = []
 		let c = 0
-		if(platform === 3 && encoding === 1) {
+		if(encoding === 'unicode_bmp') {
 			for(let i = 0; i < length; i+=2) {
 				c = u16(offset + i)
 				if(c > 31) {
@@ -128,20 +289,117 @@ function parse_tt_name(table: FontTable, buf: Uint8Array) {
 				} else { s += '^' + String.fromCodePoint(c + 64) }
 				//meh.push(c)
 			}
-			let key = `windows_unicode_${lang}`
-			if((table as any)[key] == undefined) {
-				(table as any)[key] = {}
-			}
-			if(font_name_ids[id] != undefined) {
-				(table as any)[key][font_name_ids[id]] = s
-			} else {
-				(table as any)[key][`_${id}`] = s
-			}
+		}
+		let key = `${platform}/${encoding}/${lang}`
+		if((table as any)[key] == undefined) {
+			(table as any)[key] = {}
+		}
+		if(font_name_ids[id] != undefined) {
+			(table as any)[key][font_name_ids[id]] = s
 		} else {
-			console.log('name rec with a most uncomprehensible encoding', platform, encoding, lang, id, length, offset)
+			(table as any)[key][`_${id}`] = s
 		}
 	}
 	//console.log('name table:', table)
+}
+
+function parse_cmap(table: FontTable & {cmap:any}, buf: Uint8Array) {
+	let u16 = (offset: number) => (buf[offset]<<8) + buf[offset+1]
+	let i16 = (offset: number) => ((buf[offset]>127) ? -65536 : 0) + (buf[offset]<<8) + buf[offset+1]
+	let u32 = (offset: number) => (buf[offset]*0x1000000) + (buf[offset+1]<<16) + (buf[offset+2]<<8) + buf[offset+3]
+	let version = u16(0)
+	let count = u16(2)
+	console.log(`'cmap' table version`, version, 'with', count, 'entries')
+	let cmaps:any = {}
+	table.cmap = cmaps
+	let oadd = (v:number) => { v = (v < 0) ? (v + 65536) : v; return v > 65535 ? v - 65536 : v }
+	let hex = (v:number) => v.toString(16).padStart(4,'0')
+	for(let i = 0; i < count; i++) {
+		let platform = tt_platforms[u16(4+i*8)] ?? 'unknown'
+		let plat_enc = tt_platform_encodings[platform]
+		let encoding_id = plat_enc[u16(6+i*8)] ?? `#${u16(6+i*8)}`
+		let offset = u32(8+i*8)
+		let st_format = u16(offset)
+		if(cmaps[encoding_id] !== undefined) {
+			continue
+		}
+		if(st_format == 4) {
+			let st_length = u16(offset+2)
+			let st_lang = u16(offset+4)
+			let seg_count2 = u16(offset+6)
+			let seg_count = seg_count2 >> 1
+			let search_range = u16(offset+8)
+			let entry_sel = u16(offset+10)
+			let range_shift = u16(offset+12)
+			let glyphs = new Uint32Array(65536)
+			cmaps[encoding_id] = glyphs
+			let off_endcodes = offset+14
+			let off_startcodes = off_endcodes + 2 + seg_count2
+			let off_id_delta = off_startcodes + seg_count2
+			let off_id_range_off = off_id_delta + seg_count2
+			let off_id_array = off_id_range_off + seg_count2
+			let glyph_count = 0
+			for(let i = 0; i < seg_count; i++) {
+				let o = i*2
+				let start = u16(off_startcodes+o), end = u16(off_endcodes+o)
+				let delta = i16(off_id_delta+o)
+				let range_off = u16(off_id_range_off+o)
+				let l = end - start + 1
+				if(range_off === 0) {
+					if(start == end) {
+						let gid = oadd(end + delta)
+						if(gid !== 0) { glyph_count++ }
+						glyphs[start] = gid
+						//console.log('segment', hex(end), '->', oadd(end + delta))
+					} else {
+						glyph_count += l
+						for(let c = start; c <= end; c++) {
+							glyphs[c] = oadd(c + delta)
+						}
+						//console.log('segment', hex(start), hex(end), '->', oadd(start + delta), oadd(end + delta))
+					}
+				} else {
+					let gro = off_id_range_off + o + range_off //- start
+					for(let c = 0; c < l; c++) {
+						let gid = oadd(u16(gro + c+c) + delta)
+						if(gid !== 0) {
+							glyph_count++;
+							glyphs[start + c] = gid
+						} else {
+							glyphs[start + c] = 0
+						}
+					}
+					//console.log('segment', hex(start), hex(end), 'n', l, gro - off_id_array)
+				}
+			}
+			console.log('cmap', encoding_id, 'glyph count:', glyph_count)
+		} else if(st_format == 12) {
+			let st_length = u32(offset + 4)
+			let st_lang = u32(offset + 8)
+			let st_groups = u32(offset + 12)
+			let off_map = offset + 16
+			let glyph_count = 0
+			let glyph_count_bmp = 0
+			let glyphs = new Uint32Array(0x110000)
+			cmaps[encoding_id] = glyphs
+			for(let i = 0; i < st_groups; i++) {
+				let o = i * 12
+				let start = u32(off_map + o)
+				let end = u32(off_map + o + 4)
+				let glyph_start = u32(off_map + o + 8)
+				let l = end - start + 1
+				for(let c = 0; c < l; c++) {
+					if(c + start < 0x10000) glyph_count_bmp += 1
+					glyphs[c + start] = glyph_start + c
+				}
+				glyph_count += l
+			}
+			console.log('cmap', encoding_id, 'glyph count:', glyph_count_bmp, glyph_count)
+		} else {
+			console.log(`cmap ${platform}/${encoding_id} @`, offset, 'format:', st_format)
+		}
+	}
+	//console.log('cmap', table)
 }
 
 function parse_cff1(table: FontTable, cff_buf: Uint8Array) {
@@ -226,7 +484,7 @@ function parse_cff1(table: FontTable, cff_buf: Uint8Array) {
 			}
 			return s
 		} else {
-			return '<predefined string that i am too lazy to put in the source code yet>'
+			return font_std_sids[sid]
 		}
 	}
 	console.log('CFF Top DICT count', cff_top_dict_index.count)
@@ -475,8 +733,8 @@ function parse_cff1(table: FontTable, cff_buf: Uint8Array) {
 	}
 	let last_top_dict: CFFTopDict = {} as CFFTopDict
 	cff_top_dict_index.forEach(function(buf, offset:number, index:number, length:number) {
-		console.log('CFF Top DICT #', index, 'len=', length)
 		last_top_dict = load_dict(buf, offset, length, top_dict_entries)
+		console.log('CFF Top DICT #', index, 'len=', length, last_top_dict)
 	})
 	let number_of_charstrings_we_wasted_time_on = 0
 	if(last_top_dict.charstrings != undefined) {
@@ -608,6 +866,13 @@ function parse_cff1(table: FontTable, cff_buf: Uint8Array) {
 	//
 }
 
+const parsers_for_font_things:{[i:string]:((t:FontTable, buf:Uint8Array)=>void)|undefined} = {
+	'head': parse_tt_head,
+	'name': parse_tt_name,
+	'CFF ': parse_cff1,
+	'cmap': parse_cmap,
+}
+
 export function load_font_outlines_from_ttf(font_array: ArrayBuffer) {
 	let view = new DataView(font_array)
 	let font_buf = new Uint8Array(font_array)
@@ -643,10 +908,6 @@ export function load_font_outlines_from_ttf(font_array: ArrayBuffer) {
 		}
 	}
 	let tables: FontTable[] = []
-	const parsers_for_font_things:{[i:string]:((t:FontTable, buf:Uint8Array)=>void)|undefined} = {
-		'name': parse_tt_name,
-		'CFF ': parse_cff1,
-	}
 	for(let font_index = 0; font_index < file_number_o_fonts; font_index++) {
 		let font_offset = u32(12 + (font_index * 4))
 		if((font_offset + 12) >= font_array.byteLength) {
